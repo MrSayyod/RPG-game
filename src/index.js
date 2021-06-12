5ffe26b0f8bcb6942a0aed5753aea88e5e42dc98
MrSayyod <ssayyod@gmail.com>
2021-06-12 13:47:32 -0700

    Add initial configuration and functions for Phaser

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
-console.log('hi');
-console.log('hello');
+import 'phaser'
+
+let config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+      preload: preload,
+      create: create,
+      update: update
+  }
+};
+
+let game = new Phaser.Game(config);
+
+function preload () {
+}
+
+function create () {
+}
+
+function update () {
+}

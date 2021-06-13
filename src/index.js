41a543b56969ddc4667bf193d3dcf5729100720a
MrSayyod <ssayyod@gmail.com>
2021-06-12 17:50:07 -0700

    Add background image and some grounds to the game

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import 'phaser'
+import sky from './assets/sky.png'
+import ground from './assets/platform.png'
+// import sky from './assets/sky.png'
+// import sky from './assets/sky.png'
+// import sky from './assets/sky.png'
 
 let config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+        gravity: { y: 300 },
+        debug: false
+    }
+},
   scene: {
       preload: preload,
       create: create,
@@ -11,13 +23,30 @@ let config = {
   }
 };
 
+let platforms
 let game = new Phaser.Game(config);
 
-function preload () {
+function preload ()
+{
+  this.load.image('sky', sky);
+  this.load.image('ground', ground);
+  this.load.image('star', 'assets/star.png');
+  this.load.image('bomb', 'assets/bomb.png');
+  this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
-function create () {
+function create ()
+{
+  this.add.image(400, 300, 'sky');
+  platforms = this.physics.add.staticGroup();
+
+        platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+        platforms.create(600, 400, 'ground');
+        platforms.create(50, 250, 'ground');
+        platforms.create(750, 220, 'ground');
 }
 
-function update () {
+function update ()
+{
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
+  performance: {
+    hints: false,
+  },
   devServer: {
     contentBase: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'RPG game',
     }),
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };

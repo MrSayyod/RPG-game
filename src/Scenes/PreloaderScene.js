0dfbfca2146d3d9441c1a7b2891994d770ed833b
MrSayyod <ssayyod@gmail.com>
2021-06-27 07:12:28 -0600

    Fix radix and no-plusplus errors

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -64,7 +64,7 @@ export default class PreloaderScene extends Phaser.Scene {
     assetText.setOrigin(0.5, 0.5);
 
     this.load.on('progress', (value) => {
-      percentText.setText(`${parseInt(value * 100)}%`);
+      percentText.setText(`${parseInt(value * 100, 10)}%`);
       progressBar.clear();
       progressBar.fillStyle(0xffffff, 1);
       progressBar.fillRect(250, 280, 300 * value, 30);
@@ -100,7 +100,7 @@ export default class PreloaderScene extends Phaser.Scene {
 
   ready() {
     this.scene.start('Title');
-    this.readyCount++;
+    this.readyCount += 1;
     if (this.readyCount === 2) {
       this.scene.start('Title');
     }

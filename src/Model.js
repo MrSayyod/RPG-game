017fc87a7e156c2c329d465e5c69136a5b7642e7
MrSayyod <ssayyod@gmail.com>
2021-06-27 07:00:09 -0600

    Fix no-underscore-dangle errors

--- src/Model.js
+++ src/Model.js
@@ -1,31 +1,38 @@
 export default class Model {
   constructor() {
-    this._soundOn = true;
-    this._musicOn = true;
+    // eslint-disable-next-line no-underscore-dangle
+    this._soundOn = true; // eslint-disable-next-line no-underscore-dangle
+    this._musicOn = true; // eslint-disable-next-line no-underscore-dangle
     this._bgMusicPlaying = false;
   }
 
   set musicOn(value) {
+    // eslint-disable-next-line no-underscore-dangle
     this._musicOn = value;
   }
 
   get musicOn() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._musicOn;
   }
 
   set soundOn(value) {
+    // eslint-disable-next-line no-underscore-dangle
     this._soundOn = value;
   }
 
   get soundOn() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._soundOn;
   }
 
   set bgMusicPlaying(value) {
+    // eslint-disable-next-line no-underscore-dangle
     this._bgMusicPlaying = value;
   }
 
   get bgMusicPlaying() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._bgMusicPlaying;
   }
 }
